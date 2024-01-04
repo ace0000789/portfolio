edb4ce6e31e26cb9e61642eea43aa51347ff8bc8
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2024-01-04 16:49:57 +0100

    start swiper

---
+++ source/js/init-work-swiper.js
@@ -0,0 +1,58 @@
+import Swiper from './vendor/swiper';
+
+function initWorkSwiper() {
+  const myImageSlider = new Swiper('[data-validate="swiper-work"]', {
+
+    // перетаскивание на ПК
+    simulateTouch: true,
+    // чувствительность свайпа
+    touchRatio: 1,
+    // угол срабатывания свайпа
+    touchAngle: 45,
+    // курсор перетаскивания
+    grabCursor: true,
+
+    // Управление клавиатурой
+    keyboard: {
+      // вкл/выкл
+      enbled: true,
+      // вкл/выкл только когда слайдер в пределах вьюпорта
+      onlyInViewport: true,
+    },
+
+    // Автовысота картинки
+    autoHeight: false,
+    // Бесконечный слайдер
+    loop: false,
+    // Количнство дублирущих слайдов
+    loopedSlides: 0,
+    // Свободный режим (перетаскивание по 1 фрагменту)
+    freeMod: true,
+    // Скорость переклдючения слайда
+    speed: 500,
+    // Листание
+    effect: 'slide',
+    breakpoints: {
+      1440: {
+        slidesPerView: 'auto',
+        slidesPerGroup: 2,
+        centeredSlides: false,
+        allowTouchMove: true,
+        spaceBetween: 135,
+      },
+      768: {
+        slidesPerView: 'auto',
+        spaceBetween: 35,
+      },
+
+      0: {
+        slidesPerView: 1,
+        initialSlide: 0,
+        watchSlidesProgress: true,
+        spaceBetween: 35,
+      },
+    },
+  });
+  return myImageSlider;
+}
+export {initWorkSwiper};
