0bb2c17c740a31f63cc43c4298547f13c79598e7
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2024-01-09 16:35:22 +0100

    add class scroll-lock

--- source/js/open-image.js
+++ source/js/open-image.js
@@ -23,19 +23,19 @@ function addImageOverlay() {
 
   // Добавляем обработчик клика на каждый слайд
   slides.forEach((slide, index) => {
-    slide.addEventListener('click', function() {
+    slide.addEventListener('click', function () {
       currentIndex = index;
       showImageByIndex(currentIndex);
     });
   });
 
   // Добавляем обработчик клика на оверлей для закрытия
-  overlay.addEventListener('click', function() {
+  overlay.addEventListener('click', function () {
     overlay.classList.remove('show');
   });
 
   // Добавляем обработчик события нажатия на клавишу Escape
-  document.addEventListener('keydown', function(event) {
+  document.addEventListener('keydown', function (event) {
     if (event.key === 'Escape') {
       overlay.classList.remove('show');
     }
@@ -44,7 +44,7 @@ function addImageOverlay() {
   // Добавляем обработчик события нажатия на клавишу ArrowRight для переключения на следующее изображение
   document.addEventListener('keydown', function(event) {
     if (event.key === 'ArrowRight') {
-      if(overlay.classList.contains('show')) {
+      if (overlay.classList.contains('show')) {
         currentIndex = (currentIndex + 1) % slides.length;
         showImageByIndex(currentIndex);
       }
@@ -52,29 +52,28 @@ function addImageOverlay() {
   });
 
   // Добавляем обработчик события нажатия на клавишу ArrowLeft для переключения на предыдущее изображение
-  document.addEventListener('keydown', function(event) {
+  document.addEventListener('keydown', function (event) {
     if (event.key === 'ArrowLeft') {
-      if(overlay.classList.contains('show')) {
+      if (overlay.classList.contains('show')) {
         currentIndex = (currentIndex - 1 + slides.length) % slides.length;
         showImageByIndex(currentIndex);
       }
     }
   });
 
-
   // Добавляем обработчики событий touchstart, touchmove и touchend для перелистывания на тач-устройствах
   let touchStartX = 0;
   let touchEndX = 0;
 
-  imageContainer.addEventListener('touchstart', function(event) {
+  imageContainer.addEventListener('touchstart', function (event) {
     touchStartX = event.touches[0].clientX;
   });
 
-  imageContainer.addEventListener('touchmove', function(event) {
+  imageContainer.addEventListener('touchmove', function (event) {
     touchEndX = event.touches[0].clientX;
   });
 
-  imageContainer.addEventListener('touchend', function() {
+  imageContainer.addEventListener('touchend', function () {
     const touchDiff = touchStartX - touchEndX;
     const swipeThreshold = 50; // Пороговое значение для срабатывания перелистывания
 
@@ -88,9 +87,7 @@ function addImageOverlay() {
       showImageByIndex(currentIndex);
     }
   });
-
-
 }
 
 // Используйте функцию для добавления функциональности к слайдеру
-export { addImageOverlay };
+export {addImageOverlay};
